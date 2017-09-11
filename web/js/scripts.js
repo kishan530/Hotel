81858977663086995e706364d58c5c414b10eef9
mmshivukumar <mmshivukumar@gmail.com>
2017-09-11 14:52:56 +0530

    back_up commit
    committig system

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -3,6 +3,41 @@
 "use strict";
 
 var $body = $('body');
+
+
+var nowTemp = new Date();
+var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+var checkin = $('#hotel_search_checkIn').fdatepicker({
+	
+	
+	
+	
+	onRender: function (date) {
+		//alert(date);
+		return date.valueOf() < now.valueOf() ? 'disabled' : '';
+	}
+}).on('changeDate', function (ev) {
+	if (ev.date.valueOf() > checkout.date.valueOf()) {
+		var newDate = new Date(ev.date)
+		newDate.setDate(newDate.getDate() + 1);
+		checkout.update(newDate);
+	}
+	checkin.hide();
+	$('#hotel_search_checkOut')[0].focus();
+}).data('datepicker');
+var checkout = $('#hotel_search_checkOut').fdatepicker({
+	  
+	onRender: function (date) {
+		return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+	}
+}).on('changeDate', function (ev) {
+	checkout.hide();
+}).data('datepicker');
+
+
+
+
+
 // var $head = $('head');
 // var $mainWrapper = $('#main-wrapper');
 
